474064efee7c52fceac8dfa32f444deb23405aca
klimooo <8172029+klimooo@users.noreply.github.com>
2019-03-02 03:04:55 +0100

    Update shadertoy.go

--- shadertoy/shadertoy.go
+++ shadertoy/shadertoy.go
@@ -62,7 +62,7 @@ func (st ShaderToy) Sources() (map[renderer.Stage][]renderer.Source, error) {
 		ss = append(ss, s)
 	}
 
-	glslVersion := "140"
+	glslVersion := "150"
 
 	return map[renderer.Stage][]renderer.Source{
 		renderer.StageVertex: {renderer.SourceBuf(fmt.Sprintf(`
